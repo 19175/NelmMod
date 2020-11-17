990876cc7c2ca0792b257618269272d178215b2d
19175 <73862885+19175@users.noreply.github.com>
2020-11-18 09:36:55 +1300

    Create Manifest.js
    Oooh. A folder!

---
+++ Manifests/Manifest.js
@@ -0,0 +1,7 @@
+G.DeclareManifest({
+name:'19175',
+updates:{
+	//'Example mod*':'mod.js', //we're updating any mod with a name that begins with "Example mod"
+	//you could declare other mod updates here if you wanted
+}
+});
