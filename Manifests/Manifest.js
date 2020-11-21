e7f7bcba309d92a95c8890226d05e137838ca83d
19175 <73862885+19175@users.noreply.github.com>
2020-11-22 02:35:59 +1300

    Update Manifest.js

--- Manifests/Manifest.js
+++ Manifests/Manifest.js
@@ -1,7 +1,7 @@
 G.DeclareManifest({
 name:'19175',
 updates:{
-	//'Example mod*':'mod.js', //we're updating any mod with a name that begins with "Example mod"
+	'That other dataset*':'https://cdn.jsdelivr.net/gh/19175/NelmMod@37fc38e/datasets/technacy.js', //we're updating any mod with a name that begins with "Example mod"
 	//you could declare other mod updates here if you wanted
 }
 });
